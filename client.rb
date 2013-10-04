13bbbb557b09fd5d6ecc7cc09193a8ef922db21f
Nick Doherty <nickdoherty@gmail.com>
2013-10-04 12:06:29 +1000

    Let's keep yours

--- client.rb
+++ client.rb
@@ -1,6 +1,6 @@
 class client
   def initialize
-    attr_accessor (:name :num_children :age :num_pets)
+    attr_accessor (:name, :num_children, :age, :num_pets)
     @name = "Client Name"
     @num_children = 1
     @age = 30
