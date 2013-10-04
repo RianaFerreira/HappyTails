b83dce10b54fc1bd6da4977b5df81a6da808af02
Nick Doherty <nickdoherty@gmail.com>
2013-10-04 11:09:07 +1000

    First Clients Draft

---
+++ client.rb
@@ -0,0 +1,10 @@
+class client
+  def initialize
+    attr_accessor (:name :num_children :age :num_pets)
+    @name = "Client Name"
+    @num_children = 1
+    @age = 30
+    @num_pets = []
+  end
+
+
