6c204ac1ad0b4ae2b76b3f7a16b6f76421fa33f7
Riana Ferreira <fuzzyri@Rianas-MacBook-Pro.local>
2013-10-04 16:37:42 +1000

    New animal and client instances added to an instance of shelter. Feature added to display of clients and animals.

--- client.rb
+++ client.rb
@@ -1,12 +1,12 @@
 class Client
-  attr_accessor :name, :num_children, :age, :num_pets
+  attr_accessor :name, :num_children, :age, :num_pets, :animals
 
   def initialize
-
     @name = "Client Name"
     @num_children = 1
     @age = 30
-    @num_pets = []
+    @num_pets = 1
+    @animals = []
   end
 
   def to_s
--- main.rb
+++ main.rb
@@ -3,86 +3,182 @@ require_relative 'shelter'
 require_relative 'client'
 require_relative 'animal'
 
-def get_input
-  puts "What would you like to do?\n
-        (1) Add client
-        (2) Add animal
-        (3) Adopt animal
-        (4) Put animal up for adoption
-        (5) Display all HappiTails animals
-        (6) Display all HappiTails clients"
-  menu_selection = gets.chomp
-  process_request(menu_selection)
-end
-def process_request(menu_selection)
-  case menu_selection
-  when 1
-    create_client
-    puts client.to_s
 
-  when 2
-
-
-  when 3
-
-  when 4
-
-  when 5
-
-  when 6
-
-  else
-    puts "Please pick an option 1 to 6."
+  def do_initialize
+    $shelter = Shelter.new
     get_input
   end
-end
 
-def create_client
-    #prompt them for the client information
-    puts "Enter client information"
+  def get_input
+    print "What would you like to do?\n
+          (1) Add client
+          (2) Add animal
+          (3) Adopt animal
+          (4) Put animal up for adoption
+          (5) Display all HappiTails animals
+          (6) Display all HappiTails clients\n
+          > "
+    menu_selection = gets.chomp.to_i
+    process_request(menu_selection)
+  end
+  def process_request(menu_selection)
+    case menu_selection
+    when 1
+      create_client
+      display_client
+      binding.pry
+    when 2
+      create_animal
+    when 3
+      adopt_animal
+    when 4
+      up_for_adoption
+    when 5
+      display_animal
+    when 6
+      display_client
+    else
+      puts "Please pick an option 1 to 6."
+      get_input
+    end
+  end
 
-    puts "Name"
-    name = gets.chomp
+  def create_client
+      #prompt them for the client information
+      puts "Enter client information"
 
-    puts "Age"
-    age = gets.chomp.to_i
+      print "Name:\t"
+      name = gets.chomp
 
-    puts "Number of children"
-    num_children = gets.chomp.to_i
+      print "Age:\t"
+      age = gets.chomp.to_i
 
-    puts "Number of pets"
-    num_pets = gets.chomp
+      print "Number of children:\t"
+      num_children = gets.chomp.to_i
 
-    #create instance of client
-    client = Client.new
+      print "Number of pets:\t"
+      num_pets = gets.chomp
 
-    #setup client
-    client.name = name
-    client.age = age
-    client.num_children = num_children
-    client.num_pets = num_pets
-end
+      #create instance of client
+      client = Client.new
 
+      #setup client
+      client.name = name
+      client.age = age
+      client.num_children = num_children
+      client.num_pets = num_pets
+
+      $shelter.clients << client
+
+      puts "Client #{client.name} successfully added."
+  end
+
+  def create_animal
+      #prompt them for the animal information
+      puts "Enter animal information"
+
+      print "Name:\t\t"
+      name = gets.chomp
+
+      print "Age:\t\t"
+      age = gets.chomp.to_i
+
+      print "Gender:\t\t"
+      gender = gets.chomp.to_i
+
+      print "Species:\t"
+      species = gets.chomp
+
+      #create instance of animal
+      animal = Animal.new
+
+      #setup animal
+      animal.name = name
+      animal.age = age
+      animal.gender = gender
+      animal.species = species
+
+      print "Toys:\t\t"
+      done = false
+      while !done
+        toy = gets.chomp
+        animal.toys << toy
+        print "Do you want to add another toy? (Y/N): "
+        response = gets.chomp.downcase
+
+        if response == "n"
+          done = true
+        end
+      end
+      $shelter.animals << animal
+      puts "Animal #{animal.name} successfully added."
+  end
+  def display_animal
+    #display all animals
+    puts $shelter.animals
+  end
+  def display_client
+    #display all clients
+    puts $shelter.clients
+  end
+
+
+  def adopt_animal
+    #show animals available for adoption
+    #display_animal
+    #prompt user to select an animal for adoption
+    puts "Which animal is being adopted?"
+    chosen_animal = gets.chomp
+    #animal should be assigned to a client
+    animal.owner = client
+    #remove shelter association
+    shelter.animals.delete(chosen_animal)
+  end
+
+  def up_for_adoption
+    #add new animal
+    add_animal = create_animal
+    #display existing clients
+    puts "Setup the client for the adoption:\n
+          (1) New client\n
+          (2) Select existing client"
+    response = gets.chomp.to_i
+    case response
+      when 1
+        create_client
+      when 2
+        display_client
+        puts "Enter the clients name:"
+        existing_client = gets.chomp.downcase.gsub(" ","")
+        #associate animal with shelter
+        client.animals.delete(add_animal)
+      else
+        puts "Please pick option 1 or 2"
+      end
+
+  end
+
+do_initialize
 =begin
-#create instance of shelter
-shelter1 = Shelter.new
+  #create instance of shelter
+  shelter1 = Shelter.new
 
-#create instance of animal
-animal1 = Animal. new
+  #create instance of animal
+  animal1 = Animal. new
 
-#setup animal
-animal1.name = "Fido"
-animal1.age = 2
-animal1.gender = "male"
-animal1.species = "Alsatian"
-animal1.toys = ["tennis ball", "blanky", "stick"]
+  #setup animal
+  animal1.name = "Fido"
+  animal1.age = 2
+  animal1.gender = "male"
+  animal1.species = "Alsatian"
+  animal1.toys = ["tennis ball", "blanky", "stick"]
 
-#linkages
-#link animal to shelter
-shelter1.animals << animal1
+  #linkages
+  #link animal to shelter
+  shelter1.animals << animal1
 
-#link client to shelter
-shelter1.clients << client1
+  #link client to shelter
+  shelter1.clients << client1
 
-puts shelter1.to_s
+  puts shelter1.to_s
 =end
